df9443963b4f6edf44376e2475611434ce9cbf26
joseph <josephcotterell@hotmail.co.uk>
2022-11-23 21:37:15 +0000

    added characters array and prompt to collect user data

--- script.js
+++ script.js
@@ -1,13 +1,113 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const lowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const symbols = [
+  "!",
+  "#",
+  "$",
+  "&",
+  "%",
+  "'",
+  "*",
+  "+",
+  "-",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "^",
+  "_",
+  "~",
+  "`",
+  "{",
+  "|",
+  "}",
+  ".",
+];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function generatePassword(params) {
+  // user input coming in as string
+  var userPasswordLength = prompt(
+    "Please choose a password between 8 and 128 characters"
+  );
+  // we're converting the user input to integer using parseInt
+  var pwLength = parseInt(userPasswordLength, 10);
+
+  if (!pwLength || pwLength < 8 || pwLength > 128) {
+    alert("Please choose a pw");
+    return;
+  }
+
+  // do the logic here
+}
 
 // Write password to the #password input
 function writePassword() {
+  console.log("hit");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
